5ebd1c5f6b4130e5761befe1d079e473c870d320
TomasGarrido12 <garrido.tomas12@gmail.com>
2021-09-08 22:24:56 -0300

    se cambio de nombre el ejercicio numero 4 y se hizo el commit del ejercicio 6

---
+++ ejercicioNet/modeloDeEvaluacion3/modeloDeEvaluacion3/ejercicio6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = 'a';
+            int valor;
+            int acumulador = 0;
+            do
+            {
+                Console.WriteLine("Ingrese un valor");
+                valor = int.Parse(Console.ReadLine());
+                acumulador += valor;
+
+                Console.WriteLine("¿Continuar?");
+                c = char.Parse(Console.ReadLine());
+
+            } while (ValidaS_N(c));
+
+            Console.WriteLine($"La suma de todos los valores es {acumulador}");
+            Console.ReadKey();
+        }
+
+        static bool ValidaS_N(char c)
+        {
+            bool resultado;
+
+
+            if (c == 'S')
+            {
+                resultado = true;
+            }
+            else
+            {
+                resultado = false;
+            }
+            return resultado;
+        }
+    }
+}
